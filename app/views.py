6c8a48c073a8896ae5211e89167311b2cb937be6
Chandu105 <bandaruchandana2001@gmail.com>
2023-11-09 21:34:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def chandu(request):
+    return render(request,'chandu.html')
+def mouni(request):
+    return render(request,'mouni.html')
+def vani(request):
+    return render(request,'vani.html')
+def jansi(request):
+    return render(request,'jansi.html')
